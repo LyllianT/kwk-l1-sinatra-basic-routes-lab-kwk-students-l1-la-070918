cf4faf91a96ced751d9b7462744c00685737944f
LyllianT <lilyterrific2008@gmail.com>
2018-07-19 18:57:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+def name
+  @name = name
 end
+def favorite-song
+  @favorite_song = favorite_song
+end
+def hometown
+  @hometown = hometown
+end
+class App < Sinatra::Base
+  get '/' do
+    "My name is "
+  end
+  
+  get '/' do
+    "My hometown is"
+  end
+  
+  get '/' do
+    "My favorite song is "
+  end
+end
+
